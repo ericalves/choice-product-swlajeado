3966635edb9444b3200f0fcc55a4d2d10579ad91
Eric <eric.nh@gmail.com>
2016-09-18 08:45:22 -0300

    asdasd

--- js/controllers.js
+++ js/controllers.js
@@ -10,22 +10,47 @@ function MainCtrl() {
 
 };
 
-function ProductsController($scope, $window) {
+function ProductsController($scope, $window, $uibModal) {
 
    $scope.products = [{
       name: 'Produto numero 1',
       description: 'Esse produto issso isso e aqui bla-bla...',
       image: 'https://ericalves.github.io/choice-product-swlajeado/img/products/1.jpg',
       like: false,
-      notLike: false
+      url: 'https://www.google.com',
+      notLike: false,
+        images: [
+            {
+               url: 'https://ericalves.github.io/choice-product-swlajeado/img/products/1.jpg'
+            },
+            {
+               url: 'https://ericalves.github.io/choice-product-swlajeado/img/products/2.jpg'
+            },
+            {
+               url: 'https://ericalves.github.io/choice-product-swlajeado/img/products/1.jpg'
+            }
+      ]      
    },
    {
       name: 'Produto numero 2',
       description: 'HUAH suhAns uA hsuhAUsesse produto issso isso e aqui bla-bla...',
       image: 'https://ericalves.github.io/choice-product-swlajeado/img/products/2.jpg',
       like: false,
-      notLike: false
-   }];
+      url: 'https://www.google.com',
+      notLike: false,
+        images: [
+            {
+               url: 'https://ericalves.github.io/choice-product-swlajeado/img/products/2.jpg'
+            },
+            {
+               url: 'https://ericalves.github.io/choice-product-swlajeado/img/products/1.jpg'
+            },
+            {
+               url: 'https://ericalves.github.io/choice-product-swlajeado/img/products/2.jpg'
+            }
+      ]          
+   }
+   ];
 
    $scope.currentProduct = 0;
    $scope.viewAll = function() {
@@ -43,7 +68,7 @@ function ProductsController($scope, $window) {
 
    $scope.iLike = function() {
       $scope.products[$scope.currentProduct].like = true;
-      $window.open('https://www.google.com', '_blank');
+      $window.open($scope.products[$scope.currentProduct].url, '_blank');
 
       $scope.netImage();
    };
@@ -57,9 +82,51 @@ function ProductsController($scope, $window) {
    $scope.begin = function() {
       $scope.currentProduct = 0;
    };
+
+    $scope.openProd = function (product) {
+        var modalInstance = $uibModal.open({
+            templateUrl: 'views/modal_example1.html',
+            controller: ModalInstanceCtrl,
+            resolve: {
+               params: {
+                  prod: product
+               }
+            }
+        });
+    };   
+
+
+};
+
+function ModalInstanceCtrl ($scope, $uibModalInstance, params, $window) {
+
+    $scope.ok = function () {
+        $uibModalInstance.close();
+    };
+
+    $scope.cancel = function () {
+        $uibModalInstance.dismiss('cancel');
+    };
+
+    console.log('params', params);
+    $scope.prod = params.prod;
+
+    $scope.currentImg = 0;
+
+    $scope.clickImage = function(idx) {
+      $scope.currentImg = idx;
+    }
+
+    $scope.iWant = function() {
+      $window.open($scope.prod.url, '_blank');
+      $scope.cancel();
+    }
 };
 
 angular
     .module('inspinia')
     .controller('MainCtrl', MainCtrl)
     .controller('ProductsController', ProductsController)
+    .controller('ModalInstanceCtrl', ModalInstanceCtrl)
+
+    
